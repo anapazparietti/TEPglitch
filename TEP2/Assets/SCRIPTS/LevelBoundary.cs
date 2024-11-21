4afea2abb0e02b0894618d201f9f1c29e73a19ba
ana <anapazparietti@gmail.com>
2024-11-21 19:18:18 -0300

    scripts

--- TEP2/Assets/SCRIPTS/LevelBoundary.cs
+++ TEP2/Assets/SCRIPTS/LevelBoundary.cs
@@ -7,6 +7,7 @@ public class LevelBoundary : MonoBehaviour
     public static float leftSide;
     public static float rightSide;
     public float internalLeft;
+    public float internalRight;
 
     void Update()
     {
