0f0852a293e69ee25b270ee027d8f4c60e8d7530
ana <anapazparietti@gmail.com>
2024-11-21 17:36:41 -0300

    scripts

--- TEP2/Assets/SCRIPTS/Player.cs
+++ TEP2/Assets/SCRIPTS/Player.cs
@@ -1,16 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 3; 
+
+
     void Start()
     {
-        
+    }
+    void Update()
+    { //movimiento automatico para adelante
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+        //------para moverse a los costados-----
+        //izquierda
+       if(Input.GetKey(KeyCode.A))
+       {
+        //if(this.gameObject.transform.position.x > LevelBoundary.leftSide)
+            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+       }
+       //derecha
+        if(Input.GetKey(KeyCode.D))
+       {
+            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed * -1);
+       }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
